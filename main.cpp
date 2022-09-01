d8bccff2d09823fb28fa1e8267cde12a02ef93de
sinanju <pedroubine@hotmail.com>
2022-09-01 15:47:48 -0300

    update:clean code

--- main.cpp
+++ main.cpp
@@ -9,13 +9,6 @@ int main(){
     Deck MASTER_DECK,P1,CPU;
 
     MASTER_DECK.ReadFile(P1,CPU);
-
-    cout << "\nP1" << endl;
-    P1.debug();
-    cout << "\nCPU" << endl;
-    CPU.debug();
-    cout << "\nMASTER DEBUG 2" << endl;
-    MASTER_DECK.debug();
     
     return 0;
 }
