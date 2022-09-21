277bc62bf2afe881479b5ee24141fcf676bc7133
Pedro Ubine <pedroubine@hotmail.com>
2022-09-20 23:43:41 -0300

    removed debug

--- deck.cpp
+++ deck.cpp
@@ -38,15 +38,6 @@ void Deck::Serve(Card &x)
     head = (head % Maxt) + 1;
 }
 
-void Deck::debug()
-{
-    for (int i = 0; i < count + 1; i++)
-    {
-        cout << deck_queue[i].model << "|" << deck_queue[i].curb_weigth << "|" << deck_queue[i].engine_size << "|" << deck_queue[i].horsepower << "|" << deck_queue[i].price << "|" << deck_queue[i].group << endl;
-    }
-    cout << "Cartas no baralho: " << Size() << endl;
-}
-
 bool Deck::Full()
 {
     return (count == Maxt);
